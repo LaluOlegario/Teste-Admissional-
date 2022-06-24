3d08165d03bf28e284595dcfd82c95eb7c608617
Lara Olegario <99405937+LaluOlegario@users.noreply.github.com>
2022-06-24 12:37:22 -0300

    Add files via upload

---
+++ teste 1.cpp
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+int main() {
+		
+	char desc, resp;
+	int data, prazo, hora;
+	
+	printf("\nControle de pendencias\n");
+	
+	printf("\nDigite a descricao: ");
+	scanf("%c", &desc);
+	
+	printf("\nPrazo: ");
+	scanf("%i", &prazo);
+	
+	printf("\nData: ");
+	scanf("%i", &data);
+	
+	printf("\nHorario: ");
+	scanf("%i", &hora);
+	
+	printf("\nResponsavel: ");
+	scanf("%c", &resp);
+	
+	
+	
+	
+	
+	
+	
+	
+		
+	
+		
+
+
+
+
+
+printf("\n\t");	
+		return 0;
+	}
